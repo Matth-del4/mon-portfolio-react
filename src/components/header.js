a356c89103d100e39e9cfe345be0e11431e4fbfc
MatthieuAQB <mattaqb@gmail.com>
2025-04-18 01:56:05 +0200

    Modification header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
 import React from "react";
 const header = () => {
   return (
-    <header className="bg-gray-600 text-white p-4">
-      <h1 className="text-3x1">Mon Portfolio</h1>
+    <header className="bg-dark text-white py-3 px-4">
+      <h1 className="h3 m-0">Mon Portfolio</h1>
     </header>
   );
 };
