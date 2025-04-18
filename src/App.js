12b8d6fbe0ac84f647a20ae7cf39695c49d6f352
MatthieuAQB <mattaqb@gmail.com>
2025-04-19 01:17:07 +0200

    Modif .jsx en .js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Le code starter</div>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import App from "./App.jsx";
-import reportWebVitals from "./reportWebVitals";
+import App from "./App.js";
+import reportWebVitals from "./reportWebVitals.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
