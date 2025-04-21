6915a8ea469cd46cb784570949f2cc9bcbbb4037
MatthieuAQB <mattaqb@gmail.com>
2025-04-21 09:58:32 +0200

    Ajout du div container dans accueil

--- src/pages/accueil.js
+++ src/pages/accueil.js
@@ -6,22 +6,28 @@ function Accueil() {
         <p className="lead">Développeur Web et Mobile</p>
       </header>
 
-      <section className="mb-4">
-        <h2>À propos</h2>
-        <p>
-          Je suis actuellement une formation au CEF en développement web.
-          J'apprends React, Bootstrap, etc.
-        </p>
-      </section>
+      <div className="container">
+        <div className="propos1">
+          <section className="mb-4">
+            <h2>À propos</h2>
+            <p>
+              Je suis actuellement une formation au CEF en développement web.
+              J'apprends React, Bootstrap, etc.
+            </p>
+          </section>
+        </div>
 
-      <section className="mb-4">
-        <h2>Projets</h2>
-        <ul>
-          <li>Portfolio personnel</li>
-          <li>Calculatrice en JavaScript</li>
-          <li>Mini-jeu HTML/CSS</li>
-        </ul>
-      </section>
+        <div className="projet1">
+          <section className="mb-4">
+            <h2>Projets</h2>
+            <ul>
+              <li>Portfolio personnel</li>
+              <li>Calculatrice en JavaScript</li>
+              <li>Mini-jeu HTML/CSS</li>
+            </ul>
+          </section>
+        </div>
+      </div>
 
       <footer className="text-center mt-5">
         <a href="/contact">
